7eb1449da51d9da4d4a9596fcaa718be215da5d9
Dave Bergmans <dave-bergmans@hotmail.com>
2021-09-28 09:51:27 +0200

    Add project files.

---
+++ 2122BTR/Classes/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2122BTR.Classes
+{
+    class CustomerController
+    {
+        dcBTRDataContext db;
+        public CustomerController(dcBTRDataContext db)
+        {
+            this.db = db;
+        }
+
+        public List<customer> getAllCustomers()
+        {
+            return db.customers.ToList();
+        }
+    }
+}
---
+++ 2122BTR/UserControls/ucCustomers.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2122BTR.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ucCustomers.xaml
+    /// </summary>
+    public partial class ucCustomers : UserControl
+    {
+        dcBTRDataContext db;
+        Classes.CustomerController myCC;
+        public ucCustomers(dcBTRDataContext db)
+        {
+            this.db = db;
+            this.myCC = new Classes.CustomerController(db);
+
+            InitializeComponent();
+            SetData();
+        }
+
+        void SetData()
+        {
+            dgCustomers.ItemsSource = myCC.getAllCustomers();
+        }
+    }
+}
