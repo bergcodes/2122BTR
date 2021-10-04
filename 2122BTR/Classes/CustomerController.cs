a63cb7916420f99774bb80d5c6a4c942707ce65f
Dave Bergmans <dave-bergmans@hotmail.com>
2021-10-04 10:47:58 +0200

    Week 39 - Customer CRUD

--- 2122BTR/Classes/CustomerController.cs
+++ 2122BTR/Classes/CustomerController.cs
@@ -18,5 +18,62 @@ namespace _2122BTR.Classes
         {
             return db.customers.ToList();
         }
+
+        public bool createCustomer(string sFirstname, string sLastname, string sCity, string sPhonenumber, string sEmail)
+        {
+            try
+            {
+                customer myCustomer = new customer();
+                myCustomer.firstname = sFirstname;
+                myCustomer.lastname = sLastname;
+                myCustomer.city = sCity;
+                myCustomer.phonenumber = sPhonenumber;
+                myCustomer.email = sEmail;
+                db.customers.InsertOnSubmit(myCustomer);
+                db.SubmitChanges();
+                return true;
+            }
+            catch { 
+                return false;
+            }
+        }
+
+        public bool editCustomer(int iCustomerId, string sFirstname, string sLastname, string sCity, string sPhonenumber, string sEmail)
+        {
+            try
+            {
+                customer myCustomer = (from c in db.customers
+                                       where c.customerID == iCustomerId
+                                       select c).FirstOrDefault();
+                myCustomer.firstname = sFirstname;
+                myCustomer.lastname = sLastname;
+                myCustomer.city = sCity;
+                myCustomer.phonenumber = sPhonenumber;
+                myCustomer.email = sEmail;
+                db.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool deleteCustomer(int iCustomerId)
+        {
+            try
+            {
+                customer myCustomer = (from c in db.customers
+                                       where c.customerID == iCustomerId
+                                       select c).FirstOrDefault();
+                db.customers.DeleteOnSubmit(myCustomer);
+                db.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
--- 2122BTR/UserControls/ucCustomers.xaml.cs
+++ 2122BTR/UserControls/ucCustomers.xaml.cs
@@ -33,7 +33,96 @@ namespace _2122BTR.UserControls
 
         void SetData()
         {
-            dgCustomers.ItemsSource = myCC.getAllCustomers();
+            List<customer> myCustomers = myCC.getAllCustomers();
+            dgCustomers.ItemsSource = myCustomers;
+            lblCustCount.Content = myCustomers.Count();
+        }
+
+        void EmptyInputs()
+        {
+            txtFirstname.Text = string.Empty;
+            txtLastname.Text = string.Empty;
+            txtCity.Text = string.Empty;
+            txtPhonenumber.Text = string.Empty;
+            txtEmail.Text = string.Empty;
+        }
+
+        void SetNewCustomer()
+        {
+            lblSelCustId.Content = "Nieuwe klant";
+            lblSelCustName.Content = string.Empty;
+            btnDeleteCust.Visibility = Visibility.Hidden;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (lblSelCustId.Content.ToString() == "Nieuwe klant")
+            {
+                if (myCC.createCustomer(txtFirstname.Text, txtLastname.Text, txtCity.Text, txtPhonenumber.Text, txtEmail.Text))
+                {
+                    SetData();
+                    MessageBox.Show("Klant is succesvol opgeslagen!");
+                    EmptyInputs();
+                }
+                else
+                {
+                    MessageBox.Show("Er is helaas iets misgegaan met het opslaan van de klant!");
+                }
+            }
+            else
+            {
+                if (myCC.editCustomer(Convert.ToInt32(lblSelCustId.Content), txtFirstname.Text, txtLastname.Text, txtCity.Text, txtPhonenumber.Text, txtEmail.Text))
+                {
+                    SetData();
+                    MessageBox.Show("Klant is succesvol aangepast!");
+                    EmptyInputs();
+                    SetNewCustomer();
+                }
+                else
+                {
+                    MessageBox.Show("Er is helaas iets misgegaan met het wijzigen van de klant!");
+                }
+            }
+        }
+
+        private void dgCustomers_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (dgCustomers.SelectedItem != null)
+            {
+                customer selCustomer = (customer)dgCustomers.SelectedItem;
+                txtFirstname.Text = selCustomer.firstname;
+                txtLastname.Text = selCustomer.lastname;
+                txtCity.Text = selCustomer.city;
+                txtPhonenumber.Text = selCustomer.phonenumber;
+                txtEmail.Text = selCustomer.email;
+                lblSelCustId.Content = selCustomer.customerID;
+                lblSelCustName.Content = selCustomer.lastname + ", " + selCustomer.firstname;
+                btnDeleteCust.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void btnNewCust_Click(object sender, RoutedEventArgs e)
+        {
+            SetNewCustomer();
+            EmptyInputs();
+        }
+
+        private void btnDeleteCust_Click(object sender, RoutedEventArgs e)
+        {
+            if(lblSelCustId.Content.ToString() != "Nieuwe klant")
+            {
+                if (myCC.deleteCustomer(Convert.ToInt32(lblSelCustId.Content)))
+                {
+                    SetData();
+                    MessageBox.Show("Klant is succesvol verwijderd");
+                    EmptyInputs();
+                    SetNewCustomer();
+                }
+                else
+                {
+                    MessageBox.Show("Er is helaas iets misgegaan met het verwijderen van de klant!");
+                }
+            }
         }
     }
 }
